445af66540825c84fdd37514ba82d1b81de8d26f
Stan Saner <stan.saner@hotmail.co.uk>
2023-02-05 18:30:31 +0000

    Added main menu - non-functional

--- index.js
+++ index.js
@@ -12,4 +12,105 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+// How to use inquirer
+// https://www.digitalocean.com/community/tutorials/nodejs-interactive-command-line-prompts
 
+
+// Adding an Engineer
+
+function addEngineer() {
+    console.log('Adding an engineer');
+    return;
+}
+
+// Adding an Intern
+
+function addIntern() {
+    console.log('Adding an Intern');
+    return;
+}
+
+// This is the menu prompting for additional team  members in a loop until
+// an option to finish is chosen
+
+function showMainMenu() {
+    // Show a prompt with 3 choices - engineer, intern or to finish
+    inquirer.prompt({
+      type: 'list',
+      name: 'option',
+      message: 'Please select an option.',
+      choices: [
+        {
+          name: 'Add an engineer',
+          value: 'engineer'
+        },
+        {
+          name: 'Add an Intern',
+          value: 'intern'
+        },
+        {
+          name: 'Finish adding team members',
+          value: 'exit'
+        }
+      ]
+    }).then(choice => {
+      // Call a function based on their choice
+      switch (choice.option) {
+        case 'engineer':
+          addEngineer()
+            // Call main menu again to show the menu after adding san engineer
+            .then(showMainMenu);
+          break;
+        case 'intern':
+          addIntern()
+            // Call main menu again to show the menu after displaying todos
+            .then(showMainMenu);
+          break;
+        default:
+          console.log('Thanks for using the Team Profile App!');
+          // Call render from here
+          process.exit();
+      }
+    })
+  }
+
+// The first function called from init()
+// Adds a manager object to the array of object
+// then it calls menu to add an engineer, intern or to finish
+
+function generateManager() {
+
+    inquirer.prompt([
+        {
+            name: 'name',
+            message: 'Please enter the Manager\'s name:'
+        },
+        {
+            name: 'employeeID',
+            message: 'Please enter Manager\'s employee ID:'
+        },
+        {
+            name: 'email',
+            message:'Please enter Manager\'s email address:'
+        },
+        {
+            name: 'officeNumber',
+            message: 'Please enter Manager\'s office number:'
+        }
+    ]).then(answers => {
+        console.log(answers);
+        const manager = new Manager(answers.name, answers.employeeID, answers.email, answers.officeNumber);
+        console.log(manager);
+        showMainMenu();
+    });
+ }
+
+
+ // function to initialize program
+function init() {
+    generateManager();
+}
+ 
+ // function call to initialize program
+ // Launch everything from here
+ init();
